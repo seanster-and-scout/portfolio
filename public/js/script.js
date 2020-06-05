e9553bd998f971290dd32f1bbeab47c826e71a5c
Sean Baklini <sbaklini@unm.edu>
2020-06-05 11:11:27 -0600

    Page structure complete & design is in place

--- public/js/script.js
+++ public/js/script.js
@@ -5,4 +5,12 @@ $(document).ready(function () {
         slidesToShow: 3,
         slidesToScroll: 3
     });
+
+    $(document).on('scroll', (e) => {
+        if (window.scrollY > 50) {
+            $('.navbar').addClass('navbar--scrolling')
+        } else {
+            $('.navbar').removeClass('navbar--scrolling')
+        }
+    })
 });
